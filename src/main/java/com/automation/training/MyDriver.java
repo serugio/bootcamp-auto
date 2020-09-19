d911aad6dcfe979f5ee1b377fd72e98220560318
sergio <sergiogarcia@seven4n.com>
2020-09-19 18:02:12 -0500

    adjusts to local execution

--- src/main/java/com/automation/training/MyDriver.java
+++ src/main/java/com/automation/training/MyDriver.java
@@ -14,7 +14,7 @@ public class MyDriver {
 			driver = new FirefoxDriver();
 			break;
 		case "chrome":
-			System.setProperty("webdriver.chrome.driver", "/Users/santiago.hernandez/Documents/chromedriver");
+			System.setProperty("webdriver.chrome.driver", "/Users/sergiogarciaperez/Documents/drivers/chrome/chromedriver");
 			driver = new ChromeDriver();
 			break;
 		default:
